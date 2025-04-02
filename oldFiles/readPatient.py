4bbbc2774b3f0bd93bbc20aa55e7df13662a9ae5
Stepha6 <s68106680@gmail.com>
2025-04-02 16:59:24 -0500

    Update readPatient.py

--- oldFiles/readPatient.py
+++ oldFiles/readPatient.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     # Nombre de la base de datos y la colección
     db_name = "SamplePatientService"
-    collection_name = "patients"
+    collection_name = "Patients"
 
     # Conectar a MongoDB
     collection = connect_to_mongodb(uri, db_name, collection_name)
